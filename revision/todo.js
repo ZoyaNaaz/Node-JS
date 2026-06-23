aa736e8ee2192eb45cdf061e163d5c01f25d45b8
Zoya <cse.zoya.2003@gmail.com>
2026-06-23 21:19:26 +0530

    Practice fs module in node

--- revision/todo.js
+++ revision/todo.js
@@ -1,48 +1,45 @@
-const fs = require("fs");
-const filePath = "./tasks.json";
+const fs = require("fs")
+const filePath = "./tasks.json"
 
 const loadTasks = () => {
   try {
-    const dataBuffer = fs.readFileSync(filePath);
-    const dataJSON = dataBuffer.toString();
-    return JSON.parse(dataJSON);
+    return JSON.parse(fs.readFileSync(filePath).toString())
   } catch (error) {
-    return [];
+    return []
   }
-};
+}
 const saveTasks = (tasks) => {
-  const dataJSON = JSON.stringify(tasks);
-  fs.writeFileSync(filePath, dataJSON);
-  console.log("Task Added: ", tasks);
-};
+  const dataJSON = JSON.stringify(tasks)
+  fs.writeFileSync(filePath, tasks)
+  console.log(`Task Added: ${dataJSON}`)
+}
 const addTask = (task) => {
-  const tasks = loadTasks();
-  tasks.push({ task });
-  saveTasks(tasks);
-};
+  const tasks = loadTasks()
+  tasks.push({task})
+  saveTasks(tasks)
+}
 
 const listTasks = () => {
-  const tasks = loadTasks();
+  const tasks = loadTasks()
   tasks.forEach((task, index) => {
-    console.log(`${index + 1} - ${task.task}`);
-  });
-};
+    console.log(`${index+1} - ${task.task}`)
+  })
+}
 
 const removeTask = (taskId) => {
-  const tasks = loadTasks();
-  tasks.filter((_, i) => i !== taskId - 1);
-  saveTasks(tasks);
-};
+  const tasks = loadTasks()
+  tasks = tasks.filter((_, index) => index !== taskId-1)
+}
 
-const command = process.argv[2];
-const argument = process.argv[3];
+const command = process.argv[2]
+const argument = process.argv[3]
 
-if (command === "add") {
-  addTask(argument);
-} else if (command === "list") {
-  listTasks();
-} else if (command === "remove") {
-  removeTask(parseInt(argument));
-} else {
-  console.log("command not found");
+if(command === "add"){
+  addTask(argument)
+}else if(command === "list"){
+  listTasks()
+}else if(command === "remove"){
+  removeTask(parseInt(argument))
+}else{
+  console.log("Command not found!")
 }
